0e0b62534f95595e7a1f2cea9240a9939f081da8
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-10-02 03:13:21 +0000

    JWT added fixed some problems

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import { Private } from "./pages/Private";
+
 
 //create your first component
 const Layout = () => {
@@ -22,16 +20,14 @@ const Layout = () => {
     return (
         <div>
             <BrowserRouter basename={basename}>
-                <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/" />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Private />} path="/private" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
-                </ScrollToTop>
             </BrowserRouter>
         </div>
     );
